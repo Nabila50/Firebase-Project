eecc58dc66c97b47a26d746953932b6f3b510dad
Nabila <nabila.nawshin50@gmail.com>
2025-06-05 00:04:42 +0200

    authprovider setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrHv-zyHFND6DAl-Cmlo0doGcv16lXZSI",
+  authDomain: "dragon-news-auth-6305d.firebaseapp.com",
+  projectId: "dragon-news-auth-6305d",
+  storageBucket: "dragon-news-auth-6305d.firebasestorage.app",
+  messagingSenderId: "327863797722",
+  appId: "1:327863797722:web:a0cfedf1844cdcec24e2e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
